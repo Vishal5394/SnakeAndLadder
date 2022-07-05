a86612fea085f54bf3c82227aa0503a37de7a7da
vishal5394 <vishaljadyal@gmail.com>
2022-07-05 19:02:57 +0530

    SnakeLadder2

---
+++ src/snakeAndLadder/SnakeLadder2.java
@@ -0,0 +1,13 @@
+package SnakeAndLadder;
+
+public class SnakeLadder2 {
+
+	public static void main(String[] args) {
+		int start = 0;
+		System.out.println("The start position :"+start);
+		int roll = (int) (Math.floor(Math.random() *10)%6+1);
+		System.out.println(" rolling the dice : "+ roll);
+
+	}
+
+}
